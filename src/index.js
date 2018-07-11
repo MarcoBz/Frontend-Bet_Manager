a13371531b1340939c7c3a9096235cd6c23b6735
MarcoBz <barison.marco.91@gmail.com>
2018-07-11 20:04:20 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 const des = require('./deserialize')
+require('babel-polyfill')
 const indexGroup = require('./indexGroup')
 const indexBet = require('./indexBet')
-
+const indexRecap = require('./indexRecap')
 import { u, wallet } from '@cityofzion/neon-js';
 import { str2hexstring, int2hex, hexstring2str } from '@cityofzion/neon-js/src/utils'
 import {unhexlify,hexlify} from 'binascii';
 const address = unhexlify(u.reverseHex(wallet.getScriptHashFromAddress('AK2nJJpJr6o664CWJKi1QRXjqeic2zRp8y')))
-const scriptHash = '7cbd0436107124b6d4c45edfcef99a7013e5afeb';
+const scriptHash = '2c41b17c78d76c6a9977bb63cc428a7ad6a0c7b8';
 const nos = window.NOS.V1;
 
 $(document).ready(function (){
 	nos.getAddress()
 		.then((loggedAddress) => {
 			if (loggedAddress){
+				document.getElementById('chooseGroup').innerHTML = "";
+				document.getElementById('createGroup').innerHTML = "";
 				let key = unhexlify(u.reverseHex(wallet.getScriptHashFromAddress(loggedAddress)))
 				let decodeOutput = false
 				nos.getStorage({scriptHash, key, decodeOutput})
 					.then((rawData) =>{
 						var data = des.deserialize(rawData)
-						document.getElementById('chooseGroup').innerHTML = "";
-						document.getElementById('createGroup').innerHTML = "";
 						for (let i = 0; i < data[0].length; i++){
 						  let groupElement = document.createElement("div")
 						  groupElement.className = "groupElement"
@@ -30,25 +31,6 @@ $(document).ready(function (){
 						  groupElement.appendChild(groupButton)
 						  document.getElementById('chooseGroup').appendChild(groupElement)
 						 }
-						let createNewElement = document.createElement("div")
-						createNewElement.className = "createElement col-6"
-						let createButton = document.createElement("input")
-						createButton.id = "createGroupButton"
-						createButton.type = "button"
-						createButton.className = "btn btn-outline-primary"
-						createButton.value = "Create new group"
-						createNewElement.appendChild(createButton)
-						document.getElementById('createGroup').appendChild(createNewElement)
-						let tableElement = document.createElement("div")
-						tableElement.className = "tableElement col-6"
-						let tableButton = document.createElement("input")
-						tableButton.type = "button"
-						tableButton.className = "btn btn-outline-primary"
-						tableButton.value = "Recap"
-						tableButton.id = "recapButton"
-						tableElement.appendChild(tableButton)
-						document.getElementById('createGroup').appendChild(tableElement)
-
 						$('#createGroup').on("click", "#recapButton", function (){
 							$("#recap").empty()
 							$("#main").empty()
@@ -65,20 +47,34 @@ $(document).ready(function (){
 								}
 								dataRecap.push(dataTemp)
 							}
-							indexRecap.table(dataRecap, nos, scriptHash)
+						indexRecap.table(dataRecap, nos, scriptHash)
 						});
 					})
 					////.catch ##############
+				let createNewElement = document.createElement("div")
+				createNewElement.className = "createElement col-6"
+				let createButton = document.createElement("input")
+				createButton.id = "createGroupButton"
+				createButton.type = "button"
+				createButton.className = "btn btn-outline-primary"
+				createButton.value = "Create new group"
+				createNewElement.appendChild(createButton)
+				document.getElementById('createGroup').appendChild(createNewElement)
+				let tableElement = document.createElement("div")
+				tableElement.className = "tableElement col-6"
+				let tableButton = document.createElement("input")
+				tableButton.type = "button"
+				tableButton.className = "btn btn-outline-primary"
+				tableButton.value = "Recap"
+				tableButton.id = "recapButton"
+				tableElement.appendChild(tableButton)
+				document.getElementById('createGroup').appendChild(tableElement)
 			}
 			else{
 				$('#chooseGroup').html("</div> You have to login <div>")
 			}
-
-
 		})
 		//.catch((err) => console.log(`Error: ${err.message}`)); //#######
-
-
 
 	$('#chooseGroup').on("click",".groupButton", function (){
 		$("#recap").empty()
@@ -110,29 +106,23 @@ $(document).ready(function (){
 				  			let text = indexBet.list(dataBet, betterAddress, nos, scriptHash)
 				  			$('#side').html(text)
 			  			}
-						else{
-							$('#chooseGroup').html("</div> You have to login <div>")
-						}
 			  		});
 		  		})
 				//.catch((err) => console.log(`Error: ${err.message}`));
 		  	});
-	});
+	  	});
 
-	$('#main').on("click","#createBet", function (){
-		$("#side").empty()
-			nos.getAddress()
+		$('#main').on("click","#createBet", function (){
+			$("#side").empty()
+  			nos.getAddress()
 			.then((betterAddress) => {
 				if (betterAddress){
 					betterAddress = unhexlify(u.reverseHex(wallet.getScriptHashFromAddress(betterAddress)))
 					indexBet.create(betterAddress, name, nos, scriptHash)
-				}
-				else{
-					$('#chooseGroup').html("</div> You have to login <div>")
-				}
-  		});
-		////.catch ##############			
-	});
+	  			}
+	  		});
+			////.catch ##############			
+		});
 
 
 	$('#createGroup').on("click", "#createGroupButton", function (){
@@ -140,73 +130,7 @@ $(document).ready(function (){
 			$("#side").empty()
 			$("#main").empty()
 			indexGroup.create(nos, scriptHash)
-	});
-
-	$("#main").on("click","#addAddressButton", function(){
-		let address = $(this).parents("#addAddress").find("#addAddressForm").val()
-		let nickname = $(this).parents("#addAddress").find("#addNicknameForm").val()
-		$(this).parents("#addAddress").find("#addAddressForm").val("")
-		$(this).parents("#addAddress").find("#addNicknameForm").val("")
-		let addedAddress = document.createElement("div")
-		addedAddress.className = "form-row addedAddress"
-		let div5 = document.createElement("div")
-		div5.className = "col-6"
-		let inputAddress = document.createElement("input")
-		inputAddress.className = "form-control address"
-		inputAddress.disabled = true
-		inputAddress.type = "text"
-		inputAddress.value = address
-		div5.appendChild(inputAddress)
-		addedAddress.appendChild(div5)
-		let div6 = document.createElement("div")
-		div6.className = "col-5"
-		let inputNickname = document.createElement("input")
-		inputNickname.className = "form-control nickname"
-		inputNickname.disabled = true
-		inputNickname.type = "text"
-		inputNickname.value = nickname
-		div6.appendChild(inputNickname)
-		addedAddress.appendChild(div6)
-		let div7 = document.createElement("div")
-		div7.className = "col-auto"
-		let added = document.createElement("input")
-		added.type = "button"
-		added.className = "btn btn-dark"
-		added.id =  "removeAddressButton"
-		added.innerHTML = ""
-		div7.appendChild(added)
-		addedAddress.appendChild(div7)
-		document.getElementById("createGroupForm").appendChild(addedAddress)
-	});
-	
-	$("#main").on("click","#removeAddressButton", function(){
-		$(this).parents(".addedAddress").remove()
-	});
-
-	$('#main').on("click","#invokeCreateGroup", function (){
-		let operation = ('create_league')
-		let args = []
-		let groupName = document.getElementById("groupName").value
-		$('.addedAddress').each(function(i) {
-			let addressPartecipant  = $(this).find(".address").val()
-			if (addressPartecipant){
-				args.push(addressPartecipant)
-			}
-		});
-
-		$('.addedAddress').each(function(i) {
-			let nicknamePartecipant = $(this).find(".nickname").val()
-			if (nicknamePartecipant){
-				args.push(nicknamePartecipant)
-			}
-		});
-
-		args.push(groupName)
-		nos.invoke({scriptHash, operation, args})
-    		.then((txid) => alert(`Invoke txid: ${txid} `))
-    		//.catch((err) => alert(`Error: ${err.message}`));
-  	});
-
+	  	});
 	
 	$('#recap').on("click", "#clearRecapButton", function (){
 			$("#recap").empty()
@@ -215,72 +139,18 @@ $(document).ready(function (){
 	$('#main').on("click", "#clearMainButton", function (){
 				$("#side").empty()
 				$("#main").empty()
-	});
+	  	});
 
 	$("#side").on("click", "#clearSideButton", function (){
 				$("#side").empty()
-	});
-
-		$("#side").on("click","#addProposalButton", function(){
-		let Proposal = $(this).parents("#addProposal").find("#addProposalForm").val()
-		$(this).parents("#addProposal").find("#addProposalForm").val("")
-		let addedProposal = document.createElement("div")
-		addedProposal.className = "form-row addedProposal"
-		let div5 = document.createElement("div")
-		div5.className = "col-11"
-		let inputProposal = document.createElement("input")
-		inputProposal.className = "form-control proposal"
-		inputProposal.disabled = true
-		inputProposal.type = "text"
-		inputProposal.value = Proposal
-		div5.appendChild(inputProposal)
-		addedProposal.appendChild(div5)
-		let div7 = document.createElement("div")
-		div7.className = "col-auto"
-		let added = document.createElement("input")
-		added.type = "button"
-		added.className = "btn btn-dark"
-		added.id =  "removeProposalButton"
-		added.innerHTML = ""
-		div7.appendChild(added)
-		addedProposal.appendChild(div7)
-		document.getElementById("createBetForm").appendChild(addedProposal)
-	});
-
-	$("#side").on("click","#removeProposalButton", function(){
-		$(this).parents(".addedProposal").remove()
-	});
-	$("#side").on("click","#invokeCreateBetButton", function (){
-		let operation = ('create_bet')
-		let args = []
-		args.push(player)
-		args.push(groupName)
-		args.push($("#side").find('#betText').val())
-		args.push($("#side").find('#openBlock').val())
-		args.push($("#side").find('#closeBlock').val())
-		args.push($("#side").find('#convalidateBlock').val())
-		args.push($("#side").find('#amountToBet').val())
-		args.push($("#side").find('#tokenUsed').val())
-		if ($("#canAddProposal").is(':checked')){
-			args.push('y')
-		}
-		else{
-			args.push('n')
-		}
-		$('.addedProposal').each(function(i) {
-			let addedProposal  = $(this).find(".proposal").val()
-			if (addedProposal){
-				args.push(addedProposal)
-			}
-
-		});
-		nos.invoke({scriptHash,operation,args})
-		.then((txid) => alert(`Invoke txid: ${txid} `))
-		//.catch((err) => alert(`Error: ${err.message}`));
-	});
+	  	});
 })
 
 
 
 
 
+//perche doppia virgola
+//perchè continua dopo ultimo termine
+//suddividere per ogni singolo array
+//sistemare discorso numeri e address
--- src/indexGroup.js
+++ src/indexGroup.js
@@ -46,6 +46,7 @@ function list(data, name, nos, scriptHash){
 		let decodeOutput = false
 		nos.getStorage({scriptHash, key, decodeOutput})
 			.then((rawData) => {
+				console.log('a')
 				let dataBet = des.deserialize(rawData)
 				let betStatus = indexBet.getBetStatus(dataBet)
 
@@ -76,8 +77,6 @@ function list(data, name, nos, scriptHash){
 	table.appendChild(betsTable)
 	table.appendChild(createBet)
 	
-	main.appendChild(table)
-
 	let clearMain = document.createElement("div")
 	clearMain.id = "clearMain"
 	clearMain.className = "col-2"
@@ -87,8 +86,10 @@ function list(data, name, nos, scriptHash){
 	clearMainButton.type = "button"
 	clearMainButton.value = "Clear"
 	clearMain.appendChild(clearMainButton)
-	main.appendChild(clearMain)
 	
+	
+	document.getElementById('main').appendChild(table)
+	main.appendChild(clearMain)
 }
 
 function create(nos, scriptHash){
@@ -165,7 +166,72 @@ function create(nos, scriptHash){
 	clearMainButton.type = "button"
 	clearMainButton.value = "Clear"
 	clearMain.appendChild(clearMainButton)
-	main.appendChild(clearMain)		
+	main.appendChild(clearMain)	
+	
+	$("#main").on("click","#addAddressButton", function(){
+		let address = $(this).parents("#addAddress").find("#addAddressForm").val()
+		let nickname = $(this).parents("#addAddress").find("#addNicknameForm").val()
+		$(this).parents("#addAddress").find("#addAddressForm").val("")
+		$(this).parents("#addAddress").find("#addNicknameForm").val("")
+		let addedAddress = document.createElement("div")
+		addedAddress.className = "form-row addedAddress"
+		let div5 = document.createElement("div")
+		div5.className = "col-6"
+		let inputAddress = document.createElement("input")
+		inputAddress.className = "form-control address"
+		inputAddress.disabled = true
+		inputAddress.type = "text"
+		inputAddress.value = address
+		div5.appendChild(inputAddress)
+		addedAddress.appendChild(div5)
+		let div6 = document.createElement("div")
+		div6.className = "col-5"
+		let inputNickname = document.createElement("input")
+		inputNickname.className = "form-control nickname"
+		inputNickname.disabled = true
+		inputNickname.type = "text"
+		inputNickname.value = nickname
+		div6.appendChild(inputNickname)
+		addedAddress.appendChild(div6)
+		let div7 = document.createElement("div")
+		div7.className = "col-auto"
+		let added = document.createElement("input")
+		added.type = "button"
+		added.className = "btn btn-dark"
+		added.id =  "removeAddressButton"
+		added.innerHTML = ""
+		div7.appendChild(added)
+		addedAddress.appendChild(div7)
+		document.getElementById("createGroupForm").appendChild(addedAddress)
+	});
+	$("#main").on("click","#removeAddressButton", function(){
+		$(this).parents(".addedAddress").remove()
+	});
+
+	$('#main').on("click","#invokeCreateGroup", function (){
+		let operation = ('create_group')
+		let args = []
+		let groupName = document.getElementById("groupName").value
+		$('.addedAddress').each(function(i) {
+			let addressPartecipant  = $(this).find(".address").val()
+			if (addressPartecipant){
+				args.push(addressPartecipant)
+			}
+		});
+
+		$('.addedAddress').each(function(i) {
+			let nicknamePartecipant = $(this).find(".nickname").val()
+			if (nicknamePartecipant){
+				args.push(nicknamePartecipant)
+			}
+		});
+
+		args.push(groupName)
+		nos.invoke({scriptHash, operation, args})
+    		.then((txid) => alert(`Invoke txid: ${txid} `))
+    		//.catch((err) => alert(`Error: ${err.message}`));
+  	});
+
 }
 
 module.exports.list = list
